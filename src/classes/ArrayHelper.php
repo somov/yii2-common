e1e7c00d00f66934b617adefd6566b062fa7a61d
Nikolay Somov <somov.nn@gmail.com>
2019-07-28 21:08:58 +0300

    Added ArrayHelper compileText function

--- src/classes/ArrayHelper.php
+++ src/classes/ArrayHelper.php
@@ -9,6 +9,7 @@ namespace somov\common\classes;
 
 
 use yii\helpers\BaseArrayHelper;
+use yii\helpers\StringHelper;
 
 class ArrayHelper extends BaseArrayHelper
 {
@@ -16,7 +17,7 @@ class ArrayHelper extends BaseArrayHelper
     /**
      * @param $array
      * @param string $afterItem
-     * @param array$items
+     * @param array $items
      */
     public static function insertAfter(&$array, $afterItem, $items)
     {
@@ -77,5 +78,57 @@ class ArrayHelper extends BaseArrayHelper
     }
 
 
+    /** Преобразует строку с переменными в квадратных скобках в значение из параметров $params
+     *
+     * @param string $text
+     * @param array $params
+     * @param int $count
+     * @param bool $isStripTags
+     * @return string|string[]|null
+     */
+    public static function compileText($text, $params, &$count = 0, $isStripTags = true)
+    {
+
+        $text = preg_replace_callback('/\{(.*?)\}/m', function ($m)
+        use ($text, $params, $isStripTags) {
+            $tmp = self::compileText($m[1], $params, $count, $isStripTags);
+            return ($count > 0) ? $tmp : '';
+        }, $text);
+
+        $value = preg_replace('/\s+/m', ' ', preg_replace_callback(
+            '/\[\s*(\s*[\w.]+)(\s*\,\s*(\s*[\w]+\s*)(|(?:\,\s*)([\d]+)\s*)\s*|)\]/m',
+            function ($m) use ($params, $isStripTags, &$count) {
+                $key[] = $m[1];
+
+                if (isset($key[3])) {
+                    $key[] = $key[3];
+                }
+
+                $v = self::getValue($params, $key, '');
+
+                if (!is_string($v)) {
+                    try {
+                        $v = (string)$v;
+                    } catch (\Exception $exception) {
+                        $v = '';
+                    }
+                }
+
+                if (!empty($v)) {
+                    if ($isStripTags) {
+                        $v = strip_tags($v);
+                    }
+                    $count++;
+                }
+
+                if (!empty($m[3])) {
+                    $v = StringHelper::truncate($v, $m[3], '...', null, !$isStripTags);
+                }
+
+                return $v;
+            }, $text), -1);
+
+        return $value;
+    }
 
 }
