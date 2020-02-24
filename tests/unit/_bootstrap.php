e336f45771d173bb2ec5e7d2c948e89852621e44
Nikolay Somov <somov.nn@gmail.com>
2020-02-24 14:00:37 +0200

    Refactoring package

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,15 +5,15 @@ use yii\helpers\ArrayHelper;
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-require_once __DIR__ .  '/../../../app/vendor/yiisoft/yii2/Yii.php';
-require __DIR__ .'/../../../app/vendor/autoload.php';
 
 $dir =  dirname(dirname(__DIR__));
+require_once "$dir/vendor/yiisoft/yii2/Yii.php";
+require_once "$dir/vendor/autoload.php";
 
 Yii::setAlias('@mtest', $dir  . '/tests');
 Yii::setAlias('@ext', $dir );
 
-$config = require_once __DIR__ .  '/../../../app/config/console.php';
+$config = require_once "$dir/tests/console.php";
 ArrayHelper::remove($config, 'class');
 
 (new yii\console\Application($config))->init();
